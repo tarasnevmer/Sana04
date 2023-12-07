b06991c8be3abb1796ed215ff30699c11cf24a33
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2023-12-07 20:15:10 +0200

    Додано клас GenerateMatrix для генерації матриці

---
+++ Two_dimensionalArrays/GenerateMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_dimensionalArrays
+{
+    public class GenerateMatrix
+    {
+        public static int [,] generateMatrix(int rows,  int columns, int lowerRange = -10, int upperRange = 10)
+        {
+            Random random = new Random();
+            int[,] matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0;j < columns; j++)
+                {
+                    matrix[i,j] = random.Next(lowerRange, upperRange + 1);
+                }
+            }
+            return matrix;
+        }
+    }
+}
--- Two_dimensionalArrays/Program.cs
+++ Two_dimensionalArrays/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Two_dimensionalArrays;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Введіть кількість рядків: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("Введіть кількість стовпців: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введіть нижню межу діапазону: ");
+int lowerRange = int.Parse(Console.ReadLine());
+
+Console.Write("Введіть верхню межу діапазону: ");
+int upperRange = int.Parse(Console.ReadLine());
+
+int[,] matrix = GenerateMatrix.generateMatrix(N, M, lowerRange, upperRange);
+
